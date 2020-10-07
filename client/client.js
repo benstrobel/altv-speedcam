1544f50f9460488f5d36440aee99ef8256a99097
BenediktStrobel <benedikt.strobel@tum.de>
2020-10-08 01:07:08 +0200

    Cleanup, Added User Dialog when near Speedcam, Added Useanimation (E)

--- client/client.js
+++ client/client.js
@@ -3,7 +3,8 @@ import * as game from 'natives';
 
 alt.log('Client-side has loaded!');
 
-const speedCamKey = 'X';
+const speedCamPlaceKey = 'B';
+const speedCamUseKey = 'E';
 
 alt.onServer('Server:Log', (msg1, msg2) => {
     alt.log(`Message From Server: ${msg1}`);
@@ -12,8 +13,7 @@ alt.onServer('Server:Log', (msg1, msg2) => {
 
 alt.on('keydown', (key) => {
     if (alt.gameControlsEnabled()) {
-      if (key === speedCamKey.charCodeAt(0)) {
-        alt.log("Keydown");
+      if (key === speedCamPlaceKey.charCodeAt(0)) {
         var playerPos = game.getEntityCoords(game.playerPedId());
         var player = alt.Player.local;
         var playerForwardVector = game.getEntityForwardVector(player.scriptID);
@@ -23,35 +23,68 @@ alt.on('keydown', (key) => {
           y: playerPos.y + playerForwardVector.y * 1,
           z: playerPos.z + playerForwardVector.z * 1
         }
-        alt.emitServer("serverSpawnSpeedCam", result, heading);
+        alt.emitServer("speedcam:spawn", result, player.pos, heading);
         alt.log("Client Server Spawn Cam");
       }
     }
   });
 
-alt.onServer('clientSpawnSpeedCam', (pos, heading) => {
+  alt.on('keydown', (key) => {
+    if(key === speedCamUseKey.charCodeAt(0)){
+      alt.emitServer("speedcam:use");
+    }
+  });
+
+alt.onServer('speedcam:spawn', (pos, heading) => {
   if(alt.Player.local.hasMeta("speedCamID")){
     deleteSpeedCam();
-    spawnSpeedCam();
+    spawnSpeedCam(pos, heading);
   }else{
-    spawnSpeedCam();
+    spawnSpeedCam(pos, heading);
   }
 });
 
-alt.onServer('clientDeleteSpeedCam', (speedCamID) => {
-  game.deleteEntity(alt.Player.local.getMeta("speedCamID"));
+alt.onServer('speedcam:delete', (speedCamID) => {
+  game.deleteEntity(speedCamID);
 });
 
-function deleteSpeedCam(entityID){
+alt.onServer('speedcam:showusehint', () => {
+  showNotification("", null, "CHAR_SOCIAL_CLUB", 7, "Speedcam", 'Drücke E um den Blitzer zu benutzen', 0.5);
+});
+
+alt.onServer('speedcam:usecam', () => {
+  alt.log("Starting use anim");
+  startUseAnimation();
+});
+
+
+function deleteSpeedCam(){
   game.deleteEntity(alt.Player.local.getMeta("speedCamID"));
-  alt.emitServer("serverDeleteSpeedCam", alt.Player.local.getMeta("speedCamID"));
+  alt.emitServer("speedcam:delete", alt.Player.local.getMeta("speedCamID"));
   alt.Player.local.deleteMeta("speedCamID");
 }
 
-function spawnSpeedCam(){
-  alt.log("Client Client Spawn Cam")
-    const entityID = game.createObjectNoOffset(1355733718,pos.x,pos.y,pos.z);
-    game.setEntityHeading(entityID, heading);
-    alt.Player.local.setMeta("speedCamID", entityID);
-    return entityID;
+function spawnSpeedCam(pos, heading){
+  alt.log("Client Client Spawn Cam");
+  const entityID = game.createObjectNoOffset(1355733718,pos.x,pos.y,pos.z);
+  game.setEntityHeading(entityID, heading);
+  alt.Player.local.setMeta("speedCamID", entityID);
+  return entityID;
+}
+
+function startUseAnimation(){
+  game.requestAnimDict("amb@world_human_binoculars@male@base");
+  alt.log(game.isEntityPlayingAnim(alt.Player.local.scriptID, "amb@world_human_binoculars@male@base", "base", 3));
+  game.taskPlayAnim(alt.Player.local.scriptID, "amb@world_human_binoculars@male@base", "base", 8.0, 1.0, -1, 1, 1.0, 0, 0, 0);
+  alt.log(game.isEntityPlayingAnim(alt.Player.local.scriptID, "amb@world_human_binoculars@male@base", "base", 3));  // TODO Warum hier auch false? Später benutzen für Zustandscheck
+}
+
+function showNotification(message, backgroundColor = null, notifImage = null, iconType = 0, title = "Title", subtitle = "subtitle", durationMult = 1) {
+  game.beginTextCommandThefeedPost('STRING');
+  game.addTextComponentSubstringPlayerName(message);
+  if (backgroundColor != null)
+    game.thefeedSetNextPostBackgroundColor(backgroundColor);
+  if (notifImage != null)
+    game.endTextCommandThefeedPostMessagetextTu(notifImage, notifImage, true, iconType, title, subtitle, durationMult);
+  return game.endTextCommandThefeedPostMpticker(false, true);
 }
--- server/index.mjs
+++ server/index.mjs
@@ -1,39 +1,35 @@
 import * as alt from 'alt-server';
 import chat from 'chat';
 
-console.log('==> Your Resource Has Loaded! Horray!');
-
-// Called on Server-side
-alt.on('playerConnect', (player) => {
-    // Logs to the console.
-    alt.log(`==> ${player.name} has connected.`);
-
-    // Displays message to all players.
-    chat.broadcast(`==> ${player.name} has joined.`);
-
-    // Sets the player's model.
-    player.model = 'mp_m_freemode_01';
-
-    // Spawns the player at coordinates x, y, z, with a delay of 1000 Milliseconds.
-    player.spawn(813, -279, 66, 1000);
-
-    player.giveWeapon(2210333304, 100, true);
-
-    // Emit to the player passed, the event name, the argument to send.
-    alt.emitClient(player, 'Server:Log', 'hello', 'world');
-
-    alt.emit('character:Edit', player);
-});
+var camDict = {};
 
 alt.on('playerDeath', (player) => {
     player.spawn(813, -279, 66, 5000);
     player.giveWeapon(2210333304, 100, true);
 });
 
-alt.onClient('serverSpawnSpeedCam', (player, pos, heading) => {
-    alt.emitClient(null, "clientSpawnSpeedCam", pos, heading);
+alt.onClient('speedcam:spawn', (player, pos, playerpos, heading) => {
+    alt.emitClient(null, "speedcam:spawn", pos, heading);
+    const useCircle = new alt.ColshapeCircle(playerpos.x,playerpos.y,1);
+    camDict[player.id] = useCircle;
 });
 
-alt.onClient('serverDeleteSpeedCam', (entityID) =>{
-    alt.emitClient(null, "clientDeleteSpeedCam", entityID);
+alt.onClient('speedcam:delete', (player, entityID) =>{
+    alt.emitClient(null, "speedcam:delete", entityID);
+    camDict[entityID] = undefined;
+});
+
+alt.on('entityEnterColshape', (colshape, entity) => {
+    var values = Object.keys(camDict).map(function(key){
+        return camDict[key];
+    });
+    if(entity instanceof alt.Player && values.indexOf(colshape) != -1){
+        alt.emitClient(entity, "speedcam:showusehint");
+    }
+});
+
+alt.onClient('speedcam:use', (player) => {
+    if (camDict[player.id].isEntityIn(player)){
+        alt.emitClient(player, "speedcam:usecam");
+    }
 });
